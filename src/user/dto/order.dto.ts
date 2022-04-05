e70f0569b4eeb5229b6d56c21fddae563cac331b
Suren <suren.karapetyan@ontid.com>
2022-04-05 15:00:36 +0400

    initial commit

---
+++ src/user/dto/order.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsNotEmpty, Validate } from 'class-validator';
+import { Empty } from '../../middleware/empty.customValidator';
+import { Product } from '../../db/entities/product.entity';
+
+export class orderDto {
+  @Validate(Empty, { message: 'fullName field is required' })
+  fullName: string;
+
+  @IsEmail(Empty, { message: 'email field is required' })
+  email: string;
+
+  @Validate(Empty, { message: 'Phone field is required' })
+  phone: number;
+
+  @Validate(Empty, { message: 'Address field is required' })
+  address: string;
+
+  @IsNotEmpty()
+  quantity: number;
+
+  @Validate(Empty, { message: 'Product field is required' })
+  product: Product[];
+}

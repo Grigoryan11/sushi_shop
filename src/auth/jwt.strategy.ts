ffbbb735e3162ac4774fda2f44b9da1b1a1e1a0c
Suren <suren.karapetyan@ontid.com>
2022-05-11 12:03:37 +0400

    changes

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,32 +1,18 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { InjectRepository } from '@nestjs/typeorm';
-import { userEntity } from '../db/entities/user.entity';
-import { Repository } from 'typeorm';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    @InjectRepository(userEntity)
-    private readonly userRepo: Repository<userEntity>,
-  ) {
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: 'secret',
     });
   }
-  async validate({ iat, exp, id: userId }) {
-    const timeDiff = exp - iat;
-    if (timeDiff <= 0) {
-      throw new UnauthorizedException();
-    }
-    const user = await this.userRepo.findOne(userId);
 
-    if (!user) {
-      throw new UnauthorizedException();
-    }
-    return user;
+  async validate(payload: any) {
+    return payload;
   }
 }
